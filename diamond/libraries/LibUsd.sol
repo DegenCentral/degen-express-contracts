fd38c0e3d87e2ed3514f5ce1ad6877c72a9c6bb8
Cryptoflop <max.david1007@gmail.com>
2025-03-13 13:39:39 +0100

    remove answeredInRound check

--- diamond/libraries/LibUsd.sol
+++ diamond/libraries/LibUsd.sol
@@ -33,16 +33,15 @@ library LibUsd {
 		ChainlinkOracle storage oracle = store().usdOracle;
 
 		(
-			uint80 roundId,
+			/*uint80 roundId*/,
 			int signedPrice,
 			/*uint startedAt*/,
 			uint timeStamp,
-			uint80 answeredInRound
+			/* uint80 answeredInRound*/
 		) = IChainlinkAggregatorV3(oracle.priceFeed).latestRoundData();
 
 		require(signedPrice > 0, "negative oracle price");
 		require(uint256(timeStamp) >= block.timestamp - oracle.heartBeat, "stale pricefeed");
-		require(answeredInRound >= roundId, "round not complete");
 
 		// usd oracle returns the price in 8 decimals, we want 18
 		return uint256(signedPrice) * (10 ** 10);
